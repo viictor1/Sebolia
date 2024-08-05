7eadddbe5ac129264a0609876ae0380be6579852
viictor1 <victorgaspalmeida@hotmail.com>
2024-08-05 20:23:10 -0300

    inicio backend vendedor

--- routes/livro.js
+++ routes/livro.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
+const livrosMock = [
+    {
+      titulo: 'Dois Irmões',
+      autor: 'eu',
+      editora: 'o tico',
+      ano: 2004,
+      preco: 2.50
+    },
+    {
+        titulo: 'bornia',
+        autor: 'bornia',
+        editora: 'bornia',
+        ano: 2005,
+        preco: 250
+    },
+];
+
 router.get('/', (req, res) => {
-    res.send('this is user route');
+    res.send(livrosMock);
 });
 
 module.exports = router;
