432234cd935c350c8b2fd6d255296e1587ca285a
Tiyo <eduardotiyo0@gmail.com>
2024-08-06 18:52:36 -0300

    feature: cadastro de vendedor

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const vendedorRoutes = require('./routes/vendedor');
 app.use('/livro', livroRoutes);
 app.use('/vendedor', vendedorRoutes);
 
-<<<<<<< HEAD
 const port = process.env.PORT || 3500; 
-=======
-const port = process.env.PORT || 3000; 
->>>>>>> e90617b (chore: base inicial)
 app.listen(port, () => {
     console.log(`Servidor rodando na porta: ${port}`);
 });
