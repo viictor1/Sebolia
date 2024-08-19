c38675824181e44e2bc51373dd686d57a3bb0d52
viictor1 <victorgaspalmeida@hotmail.com>
2024-08-19 20:49:21 -0300

    chore: adicionando cors

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 app.use(express.json());
+
+app.use(cors({
+    origin: '*'
+}));
 
 const livroRoutes = require('./src/routes/livro');
 
