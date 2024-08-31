8f9d23d99d9132e3f669ce13ce140e923e4b265b
Tiyo <eduardotiyo0@gmail.com>
2024-08-31 20:32:40 -0300

    feat: arrumando variaveis

--- src/service/loginService.js
+++ src/service/loginService.js
@@ -5,20 +5,21 @@ const loginRepository = require('../repository/loginRepository');
 const getLogin = (req,res)=>{
     res.render('login', {
         erro: '',
-        username: '',
+        usuario: '',
         cellphone: ''
     });
 };
 
 const createLogin = async (req, res) => {
-    const { username, password } = req.body;
-
+    const { usuario, senha } = req.body;
     try {
         // Buscar o usuário no banco de dados
-        const user = await loginRepository.getLogin(username);
-
+        const user = await loginRepository.getLogin(usuario);
+        console.log(user);
+        
         // Verifique se o usuário foi encontrado e se a senha está presente
-        if (user && user.senha && bcrypt.compareSync(password, user.senha)) {
+        if (user && user.senha && bcrypt.compareSync(senha, user.senha)) {
+            console.log(req.body);
             // Se o usuário for encontrado e a senha estiver correta
             req.session.user = user; // Assumindo que você está usando sessões para autenticação
             res.redirect('/dashboard'); // Redirecionar para o dashboard ou página protegida
