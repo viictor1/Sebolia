ac549fedfccfc5c266a005ad8bffd8d1c14ce6e3
Victor <victorgaspalmeida@hotmail.com>
2024-09-06 22:54:52 -0300

    feat: adicionando validacao de autenticacao nas rotas

--- src/routes/exemplarRoute.js
+++ src/routes/exemplarRoute.js
@@ -2,10 +2,10 @@ const exemplarService = require("../service/exemplarService");
 const express = require('express');
 const router = express.Router();
 
-router.get('/', exemplarService.getAllExemplares);
-router.get('/unico', exemplarService.getExemplarUnico);
-router.post('/', exemplarService.createExemplar);
-router.delete('/', exemplarService.deletarExemplar);
-router.put('/', exemplarService.updateExemplar);
+router.get('/', authPage(["user", "admin"]), exemplarService.getAllExemplares);
+router.get('/unico', authPage(["user", "admin"]), exemplarService.getExemplarUnico);
+router.post('/', authPage(["admin"]), exemplarService.createExemplar);
+router.delete('/', authPage(["admin"]), exemplarService.deletarExemplar);
+router.put('/', authPage(["admin"]), exemplarService.updateExemplar);
 
 module.exports = router;
